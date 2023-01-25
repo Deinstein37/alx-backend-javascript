4048a272f77e43a569f9b7bf0f15c34f1769fea1
Onolameh Daniel <101498178+Deinstein37@users.noreply.github.com>
2023-01-25 08:08:01 +0100

    Update 100-await.js

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -2,16 +2,14 @@ import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
   try {
-    return Promise.all([uploadPhoto(), createUser()])
-      .then((resps) => ({
-        photo: resps[0],
-        user: resps[1],
-      }));
-  } catch (error) {
+    return {
+      photo: await uploadPhoto(),
+      user: await createUser(),
+    };
+  } catch (err) {
     return {
       photo: null,
       user: null,
     };
   }
 }
- 
