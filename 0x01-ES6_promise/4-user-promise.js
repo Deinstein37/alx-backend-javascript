8fa6e58a9cb899a859e3ac915f635c663d62afa2
Onolameh Daniel <101498178+Deinstein37@users.noreply.github.com>
2023-01-25 07:27:09 +0100

    Update 4-user-promise.js

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,8 +1,6 @@
 export default function signUpUser(firstName, lastName) {
-  return Promise.resolve(
-    {
-      firstName,
-      lastName,
-    },
-  );
+  return new Promise((res, rej) => {
+    res({ firstName, lastName });
+    rej(new Error('An error occured'));
+  });
 }
