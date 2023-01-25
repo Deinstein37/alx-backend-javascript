fc846875748988747b4e15cdea02c1301de2167f
Onolameh Daniel <101498178+Deinstein37@users.noreply.github.com>
2023-01-25 07:22:09 +0100

    Update 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,14 +1,8 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  uploadPhoto()
-    .then((res1) => {
-      createUser()
-        .then((res2) => {
-          console.log(
-            `${res1.body} ${res2.firstName} ${res2.lastName}`,
-          );
-        })
-        .catch(() => console.log('Signup system offline'));
-    }).catch(() => console.log('Signup system offline'));
+  return Promise
+    .all([uploadPhoto(), createUser()]) // imported are functions
+    .then((data) => console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`))
+    .catch(() => console.log('Signup system offline'));
 }
