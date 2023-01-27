e816fe846313e99d4e99dbdb49a9d6d1964ff7df
Onolameh Daniel <101498178+Deinstein37@users.noreply.github.com>
2023-01-27 05:44:14 +0100

    Update 1-make_classrooms.js

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,5 +1,3 @@
-// eslint-disable-next-line import/extensions
-// eslint-disable-next-line import/no-unresolved
 import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
