4e5f2dbfc957778be5772ea09e7c6665a58a3963
Onolameh Daniel <101498178+Deinstein37@users.noreply.github.com>
2023-03-21 20:17:00 +0100

    Update server.js

--- 0x05-Node_JS_basic/full_server/server.js
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -1,13 +1,14 @@
-import express from 'express';
-import controllerRouting from './routes/index';
+const express = require('express');
+
+const router = require('./routes/index');
 
 const app = express();
 const port = 1245;
 
-controllerRouting(app);
+app.use('/', router);
+app.use('/students', router);
+app.use('/students/:major', router);
 
-app.listen(port, () => {
-  // console.log(`Example app listening at http://localhost:${port}`);
-});
+app.listen(port);
 
 export default app;
